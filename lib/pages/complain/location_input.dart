f9a6b44a49eaf7a9e48e7faec7584be7d099e45c
Akhila Suraj <akhila.abesinghe@gmail.com>
2019-09-15 01:18:53 +0530

    Implemented maps basic functions

--- lib/pages/complain/location_input.dart
+++ lib/pages/complain/location_input.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LocationInputWindow extends StatefulWidget {
   @override
@@ -6,6 +9,8 @@ class LocationInputWindow extends StatefulWidget {
 }
 
 class _LocationInputWindowState extends State<LocationInputWindow> {
+  Uri mapImageUri;
+  Uri geocodeUri;
   final TextEditingController _locationInputController =
       TextEditingController();
 
@@ -14,8 +19,14 @@ class _LocationInputWindowState extends State<LocationInputWindow> {
       controller: _locationInputController,
       textAlign: TextAlign.center,
       decoration: InputDecoration(
-        suffixIcon:
-            IconButton(icon: Icon(Icons.arrow_forward_ios), onPressed: () {}),
+        suffixIcon: IconButton(
+          icon: Icon(Icons.arrow_forward_ios),
+          onPressed: () {
+            setState(() {
+              _updateLocation();
+            });
+          },
+        ),
         fillColor: Colors.white,
         border: new OutlineInputBorder(
           borderRadius: new BorderRadius.circular(25.0),
@@ -33,11 +44,71 @@ class _LocationInputWindowState extends State<LocationInputWindow> {
     );
   }
 
+  void _buildStaticMap(String address) async {
+    if (address.length < 2) {
+      return;
+    }
+
+    geocodeUri = Uri.https(
+      'maps.googleapis.com',
+      '/maps/api/geocode/json',
+      {'address': address, 'key': 'AIzaSyBjTh5fhWEMqiDEtMYmmQyVfNYdvNcB39A'},
+    );
+    print(geocodeUri.toString());
+
+    final http.Response response = await http.get(geocodeUri.toString());
+    final decodedResponse = json.decode(response.body);
+    print(decodedResponse);
+    final formattedAddress = decodedResponse['results'][0]['formatted_address'];
+    final coords = decodedResponse['results'][0]['geometry']['location'];
+
+    setState(() {
+      _locationInputController.text = formattedAddress;
+    });
+
+    mapImageUri = Uri.https(
+      'maps.googleapis.com',
+      '/maps/api/staticmap',
+      {
+        'size': '1280x720',
+        'zoom': '18',
+        'center': '${coords['lat']},${coords['lng']}',
+        'maptype': 'roadmap',
+        'key': 'AIzaSyBjTh5fhWEMqiDEtMYmmQyVfNYdvNcB39A'
+      },
+    );
+  }
+
+  void _updateLocation() {
+    print(_locationInputController.text);
+    _buildStaticMap(_locationInputController.text);
+  }
+
+  Widget _buildCapturedMapImage() {
+    return ClipRRect(
+      borderRadius: BorderRadius.all(
+        Radius.circular(8.0),
+      ),
+      child: AspectRatio(
+        aspectRatio: 16 / 9,
+        child: Image.network(
+          mapImageUri.toString(),
+          fit: BoxFit.cover,
+          alignment: FractionalOffset.center,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         _buildLocationInputFormField(),
+        SizedBox(
+          height: 10.0,
+        ),
+        _buildCapturedMapImage(),
       ],
     );
   }
